d2fa4c6c4a79da76dca876992be9307be31f8b8f
Pavithra K R <pavithra922710@gmail.com>
2026-01-05 18:26:08 +0530

    Refine comments on missing number algorithm
    Updated comments to clarify the approach for finding the missing number.

--- CSES/Introductory/Missing Number.cpp
+++ CSES/Introductory/Missing Number.cpp
@@ -2,7 +2,7 @@
 // Initially thought of vector solution - sort the vector and find the number that is missing between 2 numbers - 0(nlogn)
 // Better solution is to store a boolean vector of size n - and mark it true if element is present in arr. element with false is answer
 // best ans is using sum of first n natural numbers property
-// find sun of first n natural numbers and subtract the sum of given numbers from it - gives the missing element
+// find sum of first n natural numbers and subtract the sum of given numbers from it - gives the missing element
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
